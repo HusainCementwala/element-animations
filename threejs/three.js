9ce67eadd5462ee88cc0369ac7e4446dd758d2bd
Husain <husaincementwalas@gmail.com>
2025-02-06 00:40:20 +0530

    made a three js eye project

--- threejs/three.js
+++ threejs/three.js
@@ -8,7 +8,11 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 //Create a Three.JS Scene
 const scene = new THREE.Scene();
 //create a new camera with positions and angles
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+
+
 
 //Keep track of the mouse position, so we can make the eye move
 let mouseX = window.innerWidth / 2;
@@ -47,6 +51,7 @@ loader.load(
 //Instantiate a new renderer and set its size
 const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 
 //Add the renderer to the DOM
 document.getElementById("container3D").appendChild(renderer.domElement);
